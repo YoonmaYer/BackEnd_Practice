28f62f99c84d53420f601d6172b22e0c7fa67711
YoonmaYer <rkddbs0813@gmail.com>
2022-09-28 09:48:57 +0900

    ~20220926

---
+++ project5/src/main/java/com/project5/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,42 @@
+package com.project5.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GenerationType;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length =500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable =false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+public void update(String title, String content){
+        this.title=title;
+        this.content=content;
+}
+}
